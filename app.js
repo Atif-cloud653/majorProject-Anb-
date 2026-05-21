4d6e891f3c52778aa3a8601688c4f129515ba25c
Atif ejaz <ejazatif90@gmail.com>
2026-05-21 20:30:00 +0530

    updated code

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema} = require("./schema.js");
+const {listingSchema , reviewSchema} = require("./schema.js");
+const Review = require("./Models/review.js");
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/WanderLust";
@@ -61,6 +62,16 @@ const validateListing = (req ,res , next)=>{
     }
 }
 
+const validateReview = (req ,res , next)=>{
+    let {error} = reviewSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400 , errMsg);
+    } else{
+        next();
+    }
+}
+
 
 
 //index Route
@@ -80,7 +91,7 @@ app.get("/listings/new" , (req ,res)=>{
 
 app.get("/listings/:id" , wrapAsync(async (req , res)=>{
     let {id} = req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs" , {listing});
 }));
 
@@ -116,6 +127,31 @@ app.delete("/listings/:id" , wrapAsync(async (req,res)=>{
     let deleteListing = await Listing.findByIdAndDelete(id);
     console.log(deleteListing);
     res.redirect("/listings");
+}));
+
+//reviews
+// post route
+
+app.post("/listings/:id/reviews" ,validateReview,  wrapAsync(async(req ,res)=>{
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+    await newReview.save();
+    await listing.save();
+
+    res.redirect(`/listings/${listing._id}`);
+}));
+
+//delete review route
+
+app.delete("/listings/:id/reviews/:reviewId" , wrapAsync(async(req, res)=>{
+    let {id , reviewId} = req.params;
+
+    await Listing.findByIdAndUpdate(id , {$pull:{reviews : reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
 }));
 
 
